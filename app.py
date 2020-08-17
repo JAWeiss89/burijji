1252d241c2087270bf4bba8ec376a6c6ce7b6600
Jorge Weiss <jorge.a.weiss@gmail.com>
2020-08-16 22:08:49 -0500

    Add name functionality

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO, send
 import os
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET')
+app.config['SECRET_KEY'] = 'aaaaa'
 
 socketio = SocketIO(app) # initialize socket io
 
 
 
 @app.route("/")
-def show_index_pg():
+def show_landing_pg():
+    """ Gets home page """
+    return render_template('landing.html')
+
+@app.route("/", methods=["POST"])
+def handle_form():
+    """ Get name and add to session """
+    name = request.form.get('name')
+    print(name)
+    session['name'] = name
+    print(session['name'])
+
+    return redirect('/chat')
+
+
+@app.route("/chat")
+def show_chat():
     """ Gets home page """
     return render_template('index.html')
 
@@ -18,9 +35,10 @@ def show_index_pg():
 @socketio.on('message')
 def message(data):
 
-    print(f"\n\n{data}\n\n")
+    # print(f"\n\n{data}\n\n")
+    edited = f"{session['name']}: {data}"
     
-    send(data, broadcast=True) #This will send the message to connected clients
+    send(edited, broadcast=True) #This will send the message to connected clients
     
 
 if __name__ == '__main__':
